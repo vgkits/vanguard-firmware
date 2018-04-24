8b13878df6853da75c2c25eb19e0097e3192bb99
Cefn Hoile <github.com@cefn.com>
2018-04-24 10:25:03 +0100

    Incorporated eol logic this time

--- utilities/shell.py
+++ utilities/shell.py
@@ -1,8 +1,9 @@
-import sys
+import sys,os
 from serial.tools import miniterm
 import config, command
 
-minicomCommand = "serial.tools.miniterm --raw --encoding ascii ${port} ${baud}"
+minicomCommand = "serial.tools.miniterm --raw --eol ${eol} --encoding ascii ${port} ${baud}"
 minicomLookup = config.hardwareConfig()
+minicomLookup.update( eol={'\n':'CR','\r\n':'CRLF'}.get(os.linesep))
 command.emulateInvocation(minicomCommand, minicomLookup)
 miniterm.main()
