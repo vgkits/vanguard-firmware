1075dc8818bf46421598d3524445a9b750c61516
Cefn Hoile <github.com@cefn.com>
2018-05-09 22:22:18 +0100

    Retired separate replserver deploy task (now built-in to modules, suitable for flashing in the long term as frozen modules to firmware). Changed imports to uploads

--- utilities/deployall.py
+++ utilities/deployall.py
@@ -3,8 +3,7 @@ from time import sleep
 
 from flash.retrieve import run as flashRetrieve
 from flash.deploy import run as flashDeploy
-from imports.deploy import run as importsDeploy
-from replserver.deploy import run as replserverDeploy
+from uploads.deploy import run as uploadsDeploy
 from wifi.deploy import run as wifiDeploy
 from main.deploy import run as mainDeploy
 
@@ -16,16 +15,13 @@ def runFlash():
     print("Deploying Micropython image")
     flashDeploy()
 
-    sleep(4) # give board and serial connection time to reset
+    sleep(10) # give board and serial connection time to reset
 
 
 def runInstall():
 
-    print("Deploying Modules")
-    importsDeploy()
-
-    print("Deploying REPLServer")
-    replserverDeploy()
+    print("Deploying Scripts")
+    uploadsDeploy()
 
     print("Configuring Default Wifi Access Point")
     wifiDeploy()
@@ -36,7 +32,7 @@ def runInstall():
 def run():
     runFlash()
     runInstall()
-    print('All modules, replserver boot and application main files deployed. Now unplug the cockle to ensure it resets \a\a\a')
+    print('All features deployed. Now unplug and replug board to ensure it resets \a\a\a')
 
 
 if __name__ == "__main__":
