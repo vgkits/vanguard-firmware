65345edc6fe546b96922aa70d55c9712319a4543
Cefn Hoile <github.com@cefn.com>
2018-05-09 18:00:29 +0100

    Used lists instead of tuples to minimise syntax complexity for learners at minor performance cost

--- modules/vgkits/color.py
+++ modules/vgkits/color.py
@@ -1,16 +1,16 @@
-red = (255, 0, 0)
-green = (0, 255, 0)
-blue = (0, 0, 255)
+red = [255, 0, 0]
+green = [0, 255, 0]
+blue = [0, 0, 255]
 
-yellow = (255, 255, 0)
-purple = (255, 0, 255)
-teal = (0, 255, 255)
+yellow = [255, 255, 0]
+purple = [255, 0, 255]
+teal = [0, 255, 255]
 
-orange = (255, 128, 0)
-pink = (255, 64, 64)
+orange = [255, 128, 0]
+pink = [255, 64, 64]
 
-white = (255, 255, 255)
-black = (0, 0, 0)
+white = [255, 255, 255]
+black = [0, 0, 0]
 
 def hsbToRgb(h, s, b): # hue, saturation, brightness
     if s == 0.0: return b, b, b
