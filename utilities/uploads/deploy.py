05219ea8e4f5b67965e7384e234dbffd69bf65e4
Cefn Hoile <github.com@cefn.com>
2018-05-09 22:23:46 +0100

    Change imports to upload to avoid python keyword

--- utilities/uploads/deploy.py
+++ utilities/uploads/deploy.py
@@ -4,8 +4,8 @@ import command
 
 def upload_imports():
     script_dir_path = os.path.dirname(os.path.realpath(__file__))
-    modules_dir_path = os.path.realpath(script_dir_path + os.path.sep + '../../modules/vgkits')
-    command.rsync(modules_dir_path, 'vgkits')
+    modules_dir_path = os.path.realpath(script_dir_path + os.path.sep + '../../modules')
+    command.rsync(modules_dir_path, '')
 
 
 def run():
