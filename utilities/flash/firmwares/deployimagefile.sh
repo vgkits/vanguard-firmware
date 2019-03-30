f81def597ebeafa03ade2980775b012a8ec3ee6b
Cefn Hoile <github.com@cefn.com>
2019-03-30 13:51:55 +0000

    Last changes before deletion

--- utilities/flash/firmwares/deployimagefile.sh
+++ utilities/flash/firmwares/deployimagefile.sh
@@ -1,2 +1,4 @@
 #!/bin/sh
-esptool.py --port /dev/ttyUSB0 --baud 1500000 write_flash --flash_mode dout 0x00000 extracted.img
+esptool.py --port /dev/ttyUSB0 --baud 1500000 erase_flash
+esptool.py --port /dev/ttyUSB0 --baud 1500000 write_flash --flash_mode dout 0x00000 vanguard-0.1.0.bin
+
--- utilities/flash/firmwares/extractimagefile.sh
+++ utilities/flash/firmwares/extractimagefile.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-esptool.py --port /dev/ttyUSB0 --baud 1500000 read_flash 0x00000 0x100000 extracted.img
+esptool.py --port /dev/ttyUSB0 --baud 1500000 read_flash 0x00000 0x100000 extracted.bin
