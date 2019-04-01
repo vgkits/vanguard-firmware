5f3d7364b673c6cc5cffef3bf7781aeead06d661
Cefn Hoile <github.com@cefn.com>
2019-04-02 00:18:54 +0100

    Added docstrings. Added pick method to choose at random from a collestion

--- modules/vgkits/random.py
+++ modules/vgkits/random.py
@@ -1,14 +1,14 @@
 from os import urandom
 from math import floor
 
-'''
-Calculates a 'uniformly' distributed random integer up to and excluding bound.
-Draws enough bytes from the random number generator for every possible bit up
-to the bound to be populated, plus an extra byte. Sequencing these bytes creates
-a random number at least 256 times larger than the target bound. This number modulus
-the range then produces an integer result within bounds. [Cefn Hoile]
-'''
 def randint(minVal, maxVal=None):
+    '''
+    Calculates a 'uniformly' distributed random integer up to and excluding bound.
+    Draws enough bytes from the random number generator for every possible bit up
+    to the bound to be populated, plus an extra byte. Sequencing these bytes creates
+    a random number at least 256 times larger than the target bound. This number modulus
+    the range then produces an integer result within bounds. [Cefn Hoile]
+    '''
     if(maxVal!=None):
         return minVal + randint(maxVal-minVal)
     else:
@@ -19,13 +19,22 @@ def randint(minVal, maxVal=None):
         val |= entry << (idx * 8)
     return val % maxVal
 
-'''
-Efficient calculation for the logarithm of val, (to the base 2), rounded 
-up to the next whole number. It rounds down to nearest integer, then unsets 
-individual bits of the int() until val is 0. The final bit unset is the largest 
-power of 2 in val, allowing us to calculate an upper bound on the logarithm. [Cefn Hoile]
-'''
+
+def pick(entries):
+    """Selects an entry from a list at random
+    :param entries: list to select from
+    :return: a single entry
+    """
+    return entries[randint(len(entries))]
+
+
 def log2approx(val):
+    '''
+    Efficient calculation for the logarithm of val, (to the base 2), rounded
+    up to the next whole number. It rounds down to nearest integer, then unsets
+    individual bits of the int() until val is 0. The final bit unset is the largest
+    power of 2 in val, allowing us to calculate an upper bound on the logarithm. [Cefn Hoile]
+    '''
     val = floor(val)
     approx = 0
     while val != 0:
