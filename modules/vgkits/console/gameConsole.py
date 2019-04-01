baf0148ab49753940c27d0774011f0ef6f482ea0
Cefn Hoile <github.com@cefn.com>
2019-04-02 00:17:50 +0100

    Simplified text-based console

--- modules/vgkits/console/gameConsole.py
+++ modules/vgkits/console/gameConsole.py
@@ -1,10 +1,51 @@
-def prompt(msg):
-    return input(msg)
+import sys
 
 
-def show(line):
-    print(line)
+def printShow(text):
+    """Causes a line of game text to appear"""
+    print(text)
 
 
-def hostGame(game, show=show, prompt=prompt):
-    game(show, prompt)
+def hostGame(gameMaker, repeat=False):
+    """
+        Hosts a game, showing output and receiving input through the text console.
+
+        It accepts a gameMaker function. This should be a generator function (a
+        function which uses the yield keyword).
+
+        The generator function will be called passing a `show` function as a parameter.
+
+        Within gameMaker, show() can be called to make lines of text appear for the user.
+
+        A question prompting the user can be passed at any point in the game sequence with a
+        yield expression. The value of the yield expression will be what the user typed
+        in response to the text prompt.
+
+        For example this would define a simple game which will run forever...
+
+        def createHelloGame(show):
+            while True:
+                show("Welcome to the game")
+                username = yield "What is your name? "
+                show("Hello " + username)
+                yield "Press enter to restart the game "
+
+        hostGame(createHelloGame)
+
+        :param function gameMaker: A generator function accepting a `showText` function and yielding user prompts.
+
+    """
+    while True:
+        try:
+            sequence = gameMaker(printShow)
+            userInput = None
+            while 1 is 1:
+                try:
+                    sys.stdout.write(b'\033c') # send a character to clear the screen
+                    userPrompt = sequence.send(userInput)
+                    userInput = input(userPrompt)
+                except StopIteration: # sequence ended
+                    break
+        finally:
+            if not repeat:
+                break
