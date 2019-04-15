a9766a17635076fe4d32fc8722aeed049e92f843
Cefn Hoile <github.com@cefn.com>
2019-04-15 23:03:39 +0100

    Renamed, commented, solved bug

---
+++ modules/vgkits/console/examples/chat.py
@@ -0,0 +1,23 @@
+posts = list()
+
+def createSequence(print):
+
+    # store this session user's name
+    myname = yield "What is your name? "
+    print("Welcome to the chatroom, " + myname)
+
+    while True:
+        # show post history
+        for name, message in posts:
+            print("<b><i>", name, "</b></i>", message)
+
+        # ask for next post
+        print("")
+        mymessage = yield "Type your message or press enter to refresh"
+
+        # handle the post
+        if mymessage is not "":
+            mypost = (myname, mymessage)
+            posts.append(mypost)
+
+        posts[:] = posts[-10:] # keep only the last 10 posts
