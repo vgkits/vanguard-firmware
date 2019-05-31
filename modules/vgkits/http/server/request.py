5a6be7bcf9b1b6c465019b68830f7c83c1a41452
Cefn Hoile <github.com@cefn.com>
2019-05-31 23:51:21 +0100

    Handle fact that Micropython has a noop behaviour for socket.makefile(). . Swallow errors from request mapping.

--- modules/vgkits/http/server/request.py
+++ modules/vgkits/http/server/request.py
@@ -39,48 +39,64 @@ def mapRequest(clientSocket, debug=False):
     contentLength = 0
     method = None
 
-    with clientSocket.makefile('rb', 0) as cl_file:
-        while True:
-            line = cl_file.readline() # consider making lowercase before processing
-            if debug:
-                print(line)
-            if not line or line == b'\r\n':
-                break
-            else:
-                try:
-                    if line.startswith(b"GET") or line.startswith(b"POST"):
-                        (method, path, version) = line.split()
-                        requestMap.update(
-                            method=method,
-                            path=path,
-                        )
-                        if method == b"GET" and b"?" in path:
-                            resource, query = path.split(b"?")
-                            requestMap.update(resource=resource)
-                            requestMap.update(params = extractParams(query))
-                        else:
-                            requestMap.update(resource=path)
-                    elif line.startswith(b"Cookie:"):
-                        _, cookieHeaderValue= line.split(b":")
-                        requestMap.update(cookies = extractCookies(cookieHeaderValue))
-                    elif line.startswith(b"Content-Type:"):
-                        contentType = line.split(b":")[-1]
-                        requestMap.update(contentType=contentType)
-                    elif line.startswith(b"Content-Length:"):
-                        contentLength = line.split(b":")[-1]
-                        contentLength = int(contentLength)
-                        requestMap.update(contentLength=contentLength)
-                    elif line.startswith(b"Purpose: prefetch"):
-                        requestMap.update(prefetch=True)
-                except ValueError:
-                    pass
-        if method == b"POST":
-            if b"application/x-www-form-urlencoded" in contentType and contentLength > 0: # extract keypairs from body
-                line = cl_file.read(contentLength) # add contentLength check and read only enough bytes
+    clFile = clientSocket.makefile('rb', 0)
+    try:
+        try:
+            while True:
+                line = clFile.readline() # consider making lowercase before processing
                 if debug:
                     print(line)
-                requestMap.update(params = extractParams(line))
-            else:
-                raise BadRequestException("POST not x-www-form-urlencoded")
+                if not line or line == b'\r\n':
+                    break
+                else:
+                    try:
+                        if line.startswith(b"GET") or line.startswith(b"POST"):
+                            (method, path, version) = line.split()
+                            requestMap.update(
+                                method=method,
+                                path=path,
+                            )
+                            if method == b"GET" and b"?" in path:
+                                resource, query = path.split(b"?")
+                                requestMap.update(resource=resource)
+                                requestMap.update(params = extractParams(query))
+                            else:
+                                requestMap.update(resource=path)
+                        elif line.startswith(b"Cookie:"):
+                            _, cookieHeaderValue= line.split(b":")
+                            requestMap.update(cookies = extractCookies(cookieHeaderValue))
+                        elif line.startswith(b"Content-Type:"):
+                            contentType = line.split(b":")[-1]
+                            requestMap.update(contentType=contentType)
+                        elif line.startswith(b"Content-Length:"):
+                            contentLength = line.split(b":")[-1]
+                            contentLength = int(contentLength) # TODO limit contentLength
+                            requestMap.update(contentLength=contentLength)
+                        elif line.startswith(b"Purpose: prefetch"):
+                            requestMap.update(prefetch=True)
+                    except ValueError:
+                        pass
+            if method == b"POST":
+                if b"application/x-www-form-urlencoded" in contentType and contentLength > 0: # extract keypairs from body
+                    line = clFile.read(contentLength)
+                    if debug:
+                        print(line)
+                    requestMap.update(params = extractParams(line))
+                else:
+                    raise BadRequestException("POST not x-www-form-urlencoded")
 
-        return requestMap
+        finally:
+            import sys
+            if hasattr(sys, 'implementation'): # mpy or py3
+                name = sys.implementation.name
+                if name == "micropython" or name == "circuitpython":
+                    pass # s.makefile() was a no-op, closing file will close socket
+                else:
+                    clFile.close() # file is separate resource, can be closed
+
+    except Exception as e:
+        print("Exception in " + __name__)
+        print(repr(e))
+
+    return requestMap
+
