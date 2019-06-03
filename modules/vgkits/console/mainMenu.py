ee9bd75f775d2efd8ab0c6c9e1fc0aba957cb2e6
Cefn Hoile <github.com@cefn.com>
2019-06-03 01:14:12 +0100

    Retired

--- modules/vgkits/console/mainMenu.py
+++
@@ -1,8 +0,0 @@
-from vgkits.console.webGameConsole import hostGame
-from vgkits.console.examples.menu import createSequence
-
-def run():
-    hostGame(createSequence, port=8080, repeat=True, debug=False)
-
-if __name__ == "__main__":
-    run()
